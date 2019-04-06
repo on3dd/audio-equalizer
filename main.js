6ebb6e6b790afcdf3ef598ff2baac4ad2c275bfe
on3dd <onedeadwave@gmail.com>
2019-04-06 23:28:06 +1000

    Updated styles

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ let fft;
 
 let w;
 
+function drawEq() {
+  clear();
+  let spectrum = fft.linAverages(64);
+  noStroke();
+  fill('rgba(255,30,30, .75)'); 
+  for (let i = 0; i< spectrum.length; i++){
+    let amp = spectrum[i];
+    let y = map(amp, 0, 256, height, 0);
+    let h = -height + map(amp, 0, 255, height, 0);
+    if ((h >= -2)) {
+      if (h > 2*(-height + map(spectrum[i-1], 0, 255, height, 0))) {
+        h = 0.5*(-height + map(spectrum[i-1], 0, 255, height, 0)) - 2;
+      }
+      else {
+        h = -2;
+      }
+    }
+    rect(i * w, height, w - 3, h);
+  }
+}
+
 function preload() {
   song = loadSound('assets/feint-time-bomb.mp3');
 }
@@ -18,22 +39,11 @@ function setup() {
 }
 
 function draw(){
-  background(0);
 
   // let spectrum = fft.analyze();
   fft.analyze();
   let spectrum = fft.linAverages(64);
-  noStroke();
-  fill(255,30,30); 
-  for (let i = 0; i< spectrum.length; i++){
-    let amp = spectrum[i];
-    let y = map(amp, 0, 256, height, 0);
-    let h = -height + map(amp, 0, 255, height, 0);
-    if (h >= -2) {
-      h = -2;
-    }
-    rect(i * w, height, w - 3, h);
-  }
+  drawEq();
 }
 
 function togglePlay() {
